04b3013a6a4ef8f54d291348351826c69e749a4b
Brilliantkid87 <brilliantkid87@gmail.com>
2023-06-10 20:11:27 +0700

    3. deploy-client

--- client/src/config/api.js
+++ client/src/config/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const API = axios.create({
-    baseURL: process.env.REACT_APP_BASEURL,
+    baseURL: process.env.REACT_APP_BASE_URL,
 });
 
 // Set Authorization Token Header
