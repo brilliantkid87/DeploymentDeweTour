ff2e50c4741415ab2e3136bcc81b23bedf8b4fef
Brilliantkid87 <brilliantkid87@gmail.com>
2023-06-10 19:42:21 +0700

    1. deploy

--- server/repositories/transaction.go
+++ server/repositories/transaction.go
@@ -92,7 +92,7 @@ func (r *repository) UpdateTransaction(status string, orderId int) (models.Trans
 	if status != transaction.Status && status == "success" {
 		var trip models.Trip
 		r.db.First(&trip, transaction.Trip.ID)
-		trip.Quota = trip.Quota - 1
+		trip.Quota = trip.Quota - transaction.CounterQty
 		r.db.Save(&trip)
 	}
 
